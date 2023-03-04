ff9dbd633b5e6098fc82dad41a9e7080c165c6c9
kded <kded300@gmail.com>
2023-03-04 11:09:37 +0000

    Initial Commit

---
+++ src/main/java/com/ondeviceresearch/vendingmachineapi/VendingMachineApiApplication.java
@@ -0,0 +1,13 @@
+package com.ondeviceresearch.vendingmachineapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendingMachineApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VendingMachineApiApplication.class, args);
+    }
+
+}

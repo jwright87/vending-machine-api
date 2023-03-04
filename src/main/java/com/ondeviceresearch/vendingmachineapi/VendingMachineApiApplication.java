01401244fd9e95dfa8fd7c52face945faf3a7980
kded <kded300@gmail.com>
2023-03-04 19:32:57 +0000

    Adds bean declaration to main class

--- src/main/java/com/ondeviceresearch/vendingmachineapi/VendingMachineApiApplication.java
+++ src/main/java/com/ondeviceresearch/vendingmachineapi/VendingMachineApiApplication.java
@@ -1,13 +1,22 @@
 package com.ondeviceresearch.vendingmachineapi;
 
+import com.ondeviceresearch.vendingmachineapi.config.ConfigData;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VendingMachineApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(VendingMachineApiApplication.class, args);
+    }
+
+
+
+    @Bean
+    public ConfigData configData() {
+        return ConfigData.createFromConfigFile();
     }
 
 }
